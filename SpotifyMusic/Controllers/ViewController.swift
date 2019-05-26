c925b8c1999cbdf4cf497721abb7ec871c7701df
Duy Bảo <duybao05123@gmail.com>
2019-05-26 07:38:36 +0700

    First commit

---
+++ SpotifyMusic/Controllers/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  SpotifyMusic
+//
+//  Created by Bao Duy on 5/26/19.
+//  Copyright © 2019 Bao Duy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    let testGit:Int = 0
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
